f06f7a5de95931d1dfb2916679d37908bb79c8c8
verilog <bryant90424@gmail.com>
2025-07-02 09:41:24 -0700

    25_0703_03

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+echo "update playV"
 cp ./playV.desktop /home/verilog/.local/share/applications/
 update-desktop-database ~/.local/share/applications
 desktop-file-validate ~/.local/share/applications/playV.desktop
