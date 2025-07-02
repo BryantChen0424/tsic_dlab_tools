5f642ac5b2234ba572fab1074422f1d7f60aa209
verilog <bryant90424@gmail.com>
2025-07-02 09:44:12 -0700

    25_0703_04

--- update.sh
+++ update.sh
@@ -3,4 +3,3 @@ cp ./playV.desktop /home/verilog/.local/share/applications/
 update-desktop-database ~/.local/share/applications
 desktop-file-validate ~/.local/share/applications/playV.desktop
 killall -SIGUSR1 gnome-shell
-gtk-launch playV
