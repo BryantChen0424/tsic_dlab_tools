fb2cbc5f830baf0a36e08298b1a229ede67b4df4
verilog <bryant90424@gmail.com>
2025-07-03 02:17:27 -0700

    25_0703_15

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 echo "update playV"
-cp ./playV.desktop /home/verilog/.local/share/applications/
+cp $DLAB_ROOT/.private/playV_dev/playV.desktop /home/verilog/.local/share/applications/
 update-desktop-database ~/.local/share/applications
 desktop-file-validate ~/.local/share/applications/playV.desktop
 killall -SIGUSR1 gnome-shell
